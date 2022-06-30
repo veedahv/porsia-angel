b623989fc5b4e8a199b5dad73eeb539eaa3cb9ed
Victoria Akpan <veedahv55@gmail.com>
2022-06-30 23:13:28 +0100

    ui fix

--- components/home/HeroSection.js
+++ components/home/HeroSection.js
@@ -3,7 +3,7 @@ export default function HeroSection() {
       <section className="">
         <div className="bg-[url('/assets/home-hero-bg.jpg')] bg-[#151515A1] bg-cover bg-norepeat bg-center bg-blend-overlay min-h-[90vh] w-full relative">
         </div>
-        <div className="my-10 md:pt-10 absolute md:relative bottom-[10px] left-0 right-0">
+        <div className="my-10 md:pt-14 absolute md:relative bottom-[10px] left-0 right-0">
           <div className="container xl:max-w-screen-xl mx-auto px-4 w-full md:text-center text-white md:text-dark">
             <h1 className="flex flex-col md:flex-row justify-start md:justify-center gap-5 uppercase text-5xl lg:text-7xl tracking-[2.5px]">
               <span className="font-title">
@@ -18,7 +18,7 @@ export default function HeroSection() {
             </h1>
           </div>
         </div>
-        <div className="container xl:max-w-screen-xl mx-auto my-10 md:pb-10 px-4 w-full text-center">
+        <div className="container xl:max-w-screen-xl mx-auto my-10 md:pb-14 px-4 w-full text-center">
           <button className="border border-primary bg-primary w-56 px-1 py-4 text-lg text-white text-center tracking-[2.5px]">
             GET STARTED
           </button>
